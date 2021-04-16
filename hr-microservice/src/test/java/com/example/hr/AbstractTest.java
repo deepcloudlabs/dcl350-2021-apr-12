63967fa40b4e52d51650007c04dcdb6f811552a6
deepcloudlabs <deepcloudlabs@gmail.com>
2021-04-16 10:09:58 +0300

    initial commit

---
+++ hr-microservice/src/test/java/com/example/hr/AbstractTest.java
@@ -0,0 +1,16 @@
+package com.example.hr;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+public abstract class AbstractTest {
+	@BeforeEach
+	void before() {
+		
+	}
+	
+	@BeforeAll
+	static void beforeAll() {
+		
+	}
+}
